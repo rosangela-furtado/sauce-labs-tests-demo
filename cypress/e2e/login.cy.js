48c14f11891f6bcaec201dfcbba07e38cd768297
Rosângela Furtado <ro_furtado16@hotmail.com>
2025-01-20 15:13:05 -0300

    Adiciona teste de login e configuracoes adicionais

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login Test', () => {
+  it('Should login successfully', () => {
+    // Usando o comando de login
+    cy.login('standard_user', 'secret_sauce')
+    
+    // Verifica que o login foi bem-sucedido
+    cy.url().should('include', '/inventory.html')
   })
 })
