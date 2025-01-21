bb9126ccad2a6be34095f0e9885c96e7592ea49f
Rosângela Furtado <ro_furtado16@hotmail.com>
2025-01-21 13:51:14 -0300

    Novos ajustes e integrando o Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     viewportWidth: 1280,
     viewportHeight: 720,
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
   },
-})
+});
